d50071260ce30b590abf94d78d808a26a47c251a
Bas Bossink <bas.bossink@gmail.com>
2013-02-02 03:36:46 +0100

    Initial commit
    Started with two tests.
    
    Next add tests for working with lunch break.

---
+++ PunchOut/PunchOut.fs
@@ -0,0 +1,16 @@
+﻿namespace PunchOut
+
+module PunchOut =
+
+    open System
+    open System.Globalization
+
+    let punchOut start target worked = 
+        let format = new DateTimeFormatInfo()
+        format.ShortTimePattern <- "H:m"
+        let startTime = DateTime.Parse(start, format , DateTimeStyles.AssumeLocal ||| DateTimeStyles.AllowWhiteSpaces)
+        let targetSpan = TimeSpan.FromHours(target)
+        let workedSpan = TimeSpan.FromHours(worked)
+        (startTime + (targetSpan - workedSpan)).ToString("H:m")
+
+        
---
+++ PunchOutTest/PunchOutTest.fs
@@ -0,0 +1,14 @@
+﻿module PunchOutTest.``Tests for the PunchOut module``
+
+open Xunit
+open FsUnit.Xunit
+open PunchOut.PunchOut
+
+[<Fact>]
+let ``When hours worked is equal to the target should get start time`` () =
+    punchOut "7:30" 37.0 37.0 |> should equal "7:30"
+
+
+[<Fact>]
+let ``When hours worked is an hour short of the target should return an hour past starting time`` () =
+    punchOut "7:30" 37.0 36.0 |> should equal "8:30"
